e5998fa2291427bd2f674547ae3dc522a12f7d2c
liyon <svliyon@gmail.com>
2024-08-14 14:54:14 +1000

    4.5

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -1,18 +1,28 @@
 const dummy = (blogs) => {
-    // ...
     return 1
   }
 
   const totalLikes = (blogs) => {
-    // ...
     const result = blogs.reduce((accumulator, item, index, array)=>{
         return accumulator + item.likes
     },0)
     return result
   }
 
+  const favoriteBlog = (blogs) => {
+    console.log("result")
+    const result = blogs.reduce((accumulator, item, index, array)=>{
+        return accumulator.likes > item.likes ? accumulator : item
+    },{})
+    const { title, author, likes } = result 
+    const extractedBlog = { title, author, likes }    
+    console.log(extractedBlog);    
+    return extractedBlog
+  }  
+
   
   module.exports = {
     dummy,
-    totalLikes
+    totalLikes,
+    favoriteBlog
   }
