88c2250e71e1b9c23742e4b740692edd6f74dc16
liyon <svliyon@gmail.com>
2024-08-15 10:40:52 +1000

    4.14

--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -43,37 +43,37 @@ const initialBlogs = [
     author: "Robert C. Martin",
     url: "http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html",
     likes: 2,
-  }  
+  }
 ]
 
-beforeEach(async () => {  
-    //delet all 
-    await Blog.deleteMany({})  
-    //create new
-    let blogObject = new Blog(initialBlogs[0])  
-    //save
-    await blogObject.save()  
-    //create new
-    blogObject = new Blog(initialBlogs[1])  
-    //save
-    await blogObject.save()  
-    //create new
-    blogObject = new Blog(initialBlogs[2])  
-    //save
-    await blogObject.save()  
-    //create new
-    blogObject = new Blog(initialBlogs[3])  
-    //save
-    await blogObject.save()  
-    //create new
-    blogObject = new Blog(initialBlogs[4])  
-    //save
-    await blogObject.save()    
-    //create new
-    blogObject = new Blog(initialBlogs[5])  
-    //save
-    await blogObject.save()            
-    
+beforeEach(async () => {
+  //delet all 
+  await Blog.deleteMany({})
+  //create new
+  let blogObject = new Blog(initialBlogs[0])
+  //save
+  await blogObject.save()
+  //create new
+  blogObject = new Blog(initialBlogs[1])
+  //save
+  await blogObject.save()
+  //create new
+  blogObject = new Blog(initialBlogs[2])
+  //save
+  await blogObject.save()
+  //create new
+  blogObject = new Blog(initialBlogs[3])
+  //save
+  await blogObject.save()
+  //create new
+  blogObject = new Blog(initialBlogs[4])
+  //save
+  await blogObject.save()
+  //create new
+  blogObject = new Blog(initialBlogs[5])
+  //save
+  await blogObject.save()
+
 })
 
 test('blogs are returned as json', async () => {
@@ -84,77 +84,118 @@ test('blogs are returned as json', async () => {
 })
 
 test('there are five blogs', async () => {
-    const response = await api.get('/api/blogs')
-  
-    assert.strictEqual(response.body.length, initialBlogs.length)
-  })
-  
+  const response = await api.get('/api/blogs')
+
+  assert.strictEqual(response.body.length, initialBlogs.length)
+})
+
 test('the first of the blogs is React patterns', async () => {
-    const response = await api.get('/api/blogs')
-  
-    const titles = response.body.map(e => e.title)
-    assert(titles.includes('React patterns'))
+  const response = await api.get('/api/blogs')
+
+  const titles = response.body.map(e => e.title)
+  assert(titles.includes('React patterns'))
 })
 
 test('the unique identifier is id and not _id', async () => {
-    const response = await api.get('/api/blogs')
-    const ids = response.body.map(e => e.id)
-    assert(ids)
-  })
+  const response = await api.get('/api/blogs')
+  const ids = response.body.map(e => e.id)
+  assert(ids)
+})
 
-  test('a valid blog can be added ', async () => {
-    const newBlog = {
-            title: "Free Code Camp",
-            author: "Quincy Larson",
-            url: "https://www.freecodecamp.org/news/",
-            likes: 100,
-          }
-  
-    await api
-      .post('/api/blogs')
-      .send(newBlog)
-      .expect(201)
-      .expect('Content-Type', /application\/json/)
-  
-    const response = await api.get('/api/blogs')
-  
-    const titles = response.body.map(r => r.title)
-  
-    assert.strictEqual(response.body.length, initialBlogs.length + 1)
-  
-    assert(titles.includes('Free Code Camp'))
-  })  
+test('a valid blog can be added ', async () => {
+  const newBlog = {
+    title: "Free Code Camp",
+    author: "Quincy Larson",
+    url: "https://www.freecodecamp.org/news/",
+    likes: 100,
+  }
 
-  test('if likes is missing it defaults to zero', async () => {
-    const newBlog = {
-            title: "Free Code Camp",
-            author: "Quincy Larson",
-            url: "https://www.freecodecamp.org/news/"
-          }
-//the response returns the added blog  
-    const response =     await api
+  await api
     .post('/api/blogs')
     .send(newBlog)
     .expect(201)
     .expect('Content-Type', /application\/json/)
-  
-    assert.strictEqual(response.body.likes, 0)
-  
-  })    
 
-  test('if title or url is missing we get 400 bad request', async () => {
-    const newBlog = {
-            author: "Quincy Larson",
-            url: "https://www.freecodecamp.org/news/",
-            likes : 100
-          }
-//the response returns 400 bad request
-    await api
+  const response = await api.get('/api/blogs')
+
+  const titles = response.body.map(r => r.title)
+
+  assert.strictEqual(response.body.length, initialBlogs.length + 1)
+
+  assert(titles.includes('Free Code Camp'))
+})
+
+test('if likes is missing it defaults to zero', async () => {
+  const newBlog = {
+    title: "Free Code Camp",
+    author: "Quincy Larson",
+    url: "https://www.freecodecamp.org/news/"
+  }
+  //the response returns the added blog  
+  const response = await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(201)
+    .expect('Content-Type', /application\/json/)
+
+  assert.strictEqual(response.body.likes, 0)
+
+})
+
+test('if title or url is missing we get 400 bad request', async () => {
+  const newBlog = {
+    author: "Quincy Larson",
+    url: "https://www.freecodecamp.org/news/",
+    likes: 100
+  }
+  //the response returns 400 bad request
+  await api
     .post('/api/blogs')
     .send(newBlog)
     .expect(400)
-  
-  })     
+
+})
+
+test('delete by id', async () => {
+  const newBlog = {
+    title: "Free Code Camp",
+    author: "Quincy Larson",
+    url: "https://www.freecodecamp.org/news/"
+  }
+  //the response returns the added blog  
+  const response = await api
+    .post('/api/blogs')
+    .send(newBlog)
+
+  console.log("response",response.body.id)  
+  await api
+    .delete(`/api/blogs/${response.body.id}`)
+    .expect(204)
+})
+
+test('update by id', async () => {
+  const likes = 1000
+  //if we leave blank - the new blog likes will be zero
+  const newBlog = {
+    title: "Free Code Camp",
+    author: "Quincy Larson",
+    url: "https://www.freecodecamp.org/news/"
+  }
+  //the response returns the added blog  
+  const response = await api
+    .post('/api/blogs')
+    .send(newBlog)
+  //update likes
+  newBlog.likes = likes
+  console.log("response",response.body.id) 
+  console.log("new blog", newBlog)                       
+  const updatedBlog = await api
+  .put(`/api/blogs/${response.body.id}`)
+  .send(newBlog)
+  console.log("updated",updatedBlog.body)
+  assert.strictEqual(updatedBlog.body.likes, likes )  
+
+})
 
 after(async () => {
   await mongoose.connection.close()
